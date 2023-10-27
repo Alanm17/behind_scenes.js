30864625da9bcb6319aead99616c729a0f435ed9
Muhammadaziz <133212735+Alanm17@users.noreply.github.com>
2023-10-28 00:44:19 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+'use strict';
+
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
+
+//   function printAge() {
+//     let output = `${firstName} is ${age},and you were born in ${birthYear}`;
+//     console.log(output);
+
+//     if (birthYear >= 1991 && birthYear <= 1996) {
+//       var Millenial = true;
+
+//       // Creating NEW variable with same name as
+//       // outer scope's variable
+//       const firstName = 'Aziz';
+
+//       // Reasssigning outer scope's variable
+//       const str = `Oh,and you're a Millenial,${firstName}`;
+//       console.log(str);
+//       function add(a, b) {
+//         return a + b;
+//       }
+//       output = 'NEW OUTPUT!';
+//     }
+//     console.log(output);
+//     console.log(Millenial);
+//     // console.log(add(2, 4));
+//   }
+//   printAge();
+//   return age;
+// }
+// const firstName = 'Muhammadaziz';
+// calcAge(1991);
+
+
+
+
+
+// var me = 'jonas';
+// let job = 'teacher';
+
+// const year = 1991;
+
+// //functions
+
+// console.log(addDecl(3,5));
+// // console.log(addExp(3,5));
+// console.log(addArrow(3,5));
+
+
+// function addDecl (a,b){
+//     return a + b;
+// }
+// var addExp = function (a , b) {
+//     return a + b;   
+// }
+// var addArrow = (a,b) => (a + b);
+
+// examples
+console.log(numbroducts);
+
+if(!numbroducts)deleteAll()
+var numbroducts = 0;
+function deleteAll (){
+    console.log('everthing is deleted!');  
+}
