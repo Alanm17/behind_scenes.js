95cc8f0a76859e3d717dfe0e1d3348748f5bccd3
Muhammadaziz <133212735+Alanm17@users.noreply.github.com>
2023-11-21 23:24:29 +0900

    recomitted codes

--- script.js
+++ script.js
@@ -1,66 +1,97 @@
 'use strict';
+ function calCage(birthYear){
+ const age = 2037 - birthYear;
 
-// function calcAge(birthYear) {
-//   const age = 2037 - birthYear;
+ function printAge(){
+ let output = `${firstName},you are ${age},born in ${birthYear}`;
+ console.log(output);
 
-//   function printAge() {
-//     let output = `${firstName} is ${age},and you were born in ${birthYear}`;
-//     console.log(output);
+ if(birthYear >= 1981 && birthYear <= 1996){
+     const firstName = 'steven';
+    const str = ` oh you are a millenial, ${firstName}`
+    var Millenial = true;
+    console.log(str);
+ }
+ function add(a,b){
+    return a + b;
+ }
 
-//     if (birthYear >= 1991 && birthYear <= 1996) {
-//       var Millenial = true;
+ output = 'NEw output '
+ console.log(Millenial);
+//  console.log(add(3,4));
 
-//       // Creating NEW variable with same name as
-//       // outer scope's variable
-//       const firstName = 'Aziz';
+ 
+ 
+  
+ }
+ printAge()
+ return age;
+ }
 
-//       // Reasssigning outer scope's variable
-//       const str = `Oh,and you're a Millenial,${firstName}`;
-//       console.log(str);
-//       function add(a, b) {
-//         return a + b;
-//       }
-//       output = 'NEW OUTPUT!';
-//     }
-//     console.log(output);
-//     console.log(Millenial);
-//     // console.log(add(2, 4));
-//   }
-//   printAge();
-//   return age;
+ const firstName = 'Mukhammadaziz';
+ calCage(1991);
+
+// // function lcAge(birthYear) {
+// //   const age = 2037 - birthYear;
+
+// //   function printAge() {
+// //     let output = `${firstName} is ${age},and you were born in ${birthYear}`;
+// //     console.log(output);
+
+// //     if (birthYear >= 1991 && birthYear <= 1996) {
+// //       var Millenial = true;
+
+// //       // Creating NEW variable with same name as
+// //       // outer scope's variable
+// //       const firstName = 'Aziz';
+
+// //       // Reasssigning outer scope's variable
+// //       const str = `Oh,and you're a Millenial,${firstName}`;
+// //       console.log(str);
+// //       function add(a, b) {
+// //         return a + b;
+// //       }
+// //       output = 'NEW OUTPUT!';
+// //     }
+// //     console.log(output);
+// //     console.log(Millenial);
+// //     // console.log(add(2, 4));
+// //   }
+// //   printAge();
+// //   return age;
+// // }
+// // const firstName = 'Muhammadaziz';
+// // calcAge(1991);
+
+
+
+
+
+// // var me = 'jonas';
+// // let job = 'teacher';
+
+// // const year = 1991;
+
+// // //functions
+
+// // console.log(addDecl(3,5));
+// // // console.log(addExp(3,5));
+// // console.log(addArrow(3,5));
+
+
+// // function addDecl (a,b){
+// //     return a + b;
+// // }
+// // var addExp = function (a , b) {
+// //     return a + b;   
+// // }
+// // var addArrow = (a,b) => (a + b);
+
+// // examples
+// console.log(numbroducts);
+
+// if(!numbroducts)deleteAll()
+// var numbroducts = 0;
+// function deleteAll (){
+//     console.log('everthing is deleted!');  
 // }
-// const firstName = 'Muhammadaziz';
-// calcAge(1991);
-
-
-
-
-
-// var me = 'jonas';
-// let job = 'teacher';
-
-// const year = 1991;
-
-// //functions
-
-// console.log(addDecl(3,5));
-// // console.log(addExp(3,5));
-// console.log(addArrow(3,5));
-
-
-// function addDecl (a,b){
-//     return a + b;
-// }
-// var addExp = function (a , b) {
-//     return a + b;   
-// }
-// var addArrow = (a,b) => (a + b);
-
-// examples
-console.log(numbroducts);
-
-if(!numbroducts)deleteAll()
-var numbroducts = 0;
-function deleteAll (){
-    console.log('everthing is deleted!');  
-}
