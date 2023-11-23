f02c5f0f8b3c376926a47f4fd62f8e13fdb0d87f
Muhammadaziz <133212735+Alanm17@users.noreply.github.com>
2023-11-23 23:37:40 +0900

    recomitting

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
 'use strict';
- function calCage(birthYear){
- const age = 2037 - birthYear;
+//  function calCage(birthYear){
+//  const age = 2037 - birthYear;
 
- function printAge(){
- let output = `${firstName},you are ${age},born in ${birthYear}`;
- console.log(output);
+//  function printAge(){
+//  let output = `${firstName},you are ${age},born in ${birthYear}`;
+//  console.log(output);
 
- if(birthYear >= 1981 && birthYear <= 1996){
-     var Millenial = true;
-     const firstName = 'steven';
+//  if(birthYear >= 1981 && birthYear <= 1996){
+//      var Millenial = true;
+//      const firstName = 'steven';
 
-     // Reasssigning outer scope's variable
-     output = 'NEW OUTPUT!';
+//      // Reasssigning outer scope's variable
+//      output = 'NEW OUTPUT!';
      
-    const str = ` oh you are a millenial, ${firstName}`
-    console.log(str);
+//     const str = ` oh you are a millenial, ${firstName}`
+//     console.log(str);
 
-    function add(a,b){
-       return a + b;
- }
+//     function add(a,b){
+//        return a + b;
+//  }
 
- }
-console.log(Millenial);
-console.log(output);
- }
- printAge()
- return age;
- }
+//  }
+// console.log(Millenial);
+// console.log(output);
+//  }
+//  printAge()
+//  return age;
+//  }
 
- const firstName = 'Mukhammadaziz';
- calCage(1991);
+//  const firstName = 'Mukhammadaziz';
+//  calCage(1991);
 
+// variables
+console.log(me);
+// console.log(teacher);
+// console.log(year);
+
+var me ='Mukhammadaziz'
+let job ='teacher';
+const year = 1991;
+
+
+// function decloration
+console.log(addDecl(2,5));
+console.log(addExp(2,7));
+console.log(addArrow(2,5));
+
+
+function addDecl (a,b){
+    return a+b;
+}
+
+const addExp = function(a,b){
+return a + b;
+}
+
+const addArrow = (a,b) => a+b;
 // // function lcAge(birthYear) {
 // //   const age = 2037 - birthYear;
 
