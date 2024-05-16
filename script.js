4fd7910d19fd828c3e3abaf3d301f107e88d89a5
Muhammadaziz <133212735+Alanm17@users.noreply.github.com>
2024-05-17 03:43:06 +0900

    this keyword

--- script.js
+++ script.js
@@ -27,24 +27,53 @@
 // // console.log(age);
 
 //Hoisting with variables
-console.log(true);
-// console.log(me);
-// console.log(age);
-let age = 13;
-const me = 'this';
-var agaim = true;
+// console.log(true);
+// // console.log(me);
+// // console.log(age);
+// let age = 13;
+// const me = 'this';
+// var agaim = true;
 
-// Hoisting with Functions************>
-console.log(ages(2, 3));
-// console.log(dasr(4, 5));
-// console.log(addArrow(3, 8));
-// decloration
-function ages(a, b) {
-  return a + b;
-}
-// expression
-const expre = function (a, b) {
-  return a + b;
+// // Hoisting with Functions************>
+// console.log(ages(2, 3));
+// // console.log(dasr(4, 5));
+// // console.log(addArrow(3, 8));
+// // decloration
+// function ages(a, b) {
+//   return a + b;
+// }
+// // expression
+// const expre = function (a, b) {
+//   return a + b;
+// };
+// //Array
+// const addArrow = (a, b) => a + b;
+console.log(this);
+const calAge = function (birthYear) {
+  console.log(2024 - birthYear);
+  // console.log(this);
 };
-//Array
-const addArrow = (a, b) => a + b;
+calAge(1991);
+
+const arrowAge = birthYear => {
+  console.log(2024 - birthYear);
+  // console.log(this);
+};
+
+arrowAge(2003);
+
+const alan = {
+  year: 2003,
+  occupation: 'programmer',
+  calcAge: function () {
+    // console.log(this);
+  },
+};
+alan.calcAge();
+
+let anna = {
+  year: 2003,
+};
+
+anna.calcAge = alan.calcAge;
+anna.calcAge();
